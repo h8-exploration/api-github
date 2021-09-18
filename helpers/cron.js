36da4daa7e9a0068b7e315c169a338e3c2c2310b
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-18 22:48:25 +0700

    convert to string crone pattern

--- helpers/cron.js
+++ helpers/cron.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const getCommitAndPushStat = require("./getCommitAndPushStat");
 
 const task = cron.schedule(
-	process.env.CRON_SCHEDULE,
+	String(process.env.CRON_SCHEDULE),
 	() => {
 		getCommitAndPushStat();
 	},
