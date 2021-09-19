daf303b08749e28068f44e0ab60d3b2c6bec9696
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-19 10:57:31 +0700

    add cron schedule to env

--- helpers/cron.js
+++ helpers/cron.js
@@ -1,8 +1,10 @@
 const cron = require("node-cron");
 const getCommitAndPushStat = require("./getCommitAndPushStat");
 
+const schedule = process.env.CRON_SCHEDULE || "0 0 8 * * *";
+
 const task = cron.schedule(
-	"0 0 8 * * *",
+	schedule,
 	() => {
 		getCommitAndPushStat();
 	},
