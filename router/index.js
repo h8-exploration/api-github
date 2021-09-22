9a0550d63ff0640fa9bfef7cab34e629fc6fb13e
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-22 13:58:58 +0700

    add wakatime find all endpoint

--- router/index.js
+++ router/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const github = require("./github");
 const student = require("./student");
 const sandbox = require("./sandbox");
+const wakatime = require("./wakatime");
 
 router.use("/github", github);
 router.use("/students", student);
 router.use("/sandbox", sandbox);
+router.use("/wakatime", wakatime);
 
 module.exports = router;
