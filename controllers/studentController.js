53a5306c56dab3e99b20d5d5e47c1bd8848f9ec4
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-27 18:27:04 +0700

    add upload excel endpoint for student

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,5 @@
 const StudentModel = require("../models/StudentModel");
+const ExcelJS = require("exceljs");
 
 class Student {
 	static findAll = async (req, res) => {
@@ -33,6 +34,40 @@ class Student {
 			const { payload, filter } = req.body;
 			const students = await StudentModel.updateMany(filter, payload);
 			res.status(200).json(students);
+		} catch (error) {
+			res.status(error.code || 500).json(error);
+		}
+	};
+
+	static uploadExcel = async (req, res) => {
+		try {
+			let students = [];
+			const wb = new ExcelJS.Workbook();
+			const workbook = await wb.xlsx.load(req.file.buffer);
+
+			workbook.eachSheet((sheet, id) => {
+				sheet.eachRow((row, rowIndex) => {
+					const payload = {
+						name: row.values[2],
+						email: row.values[3],
+						github: {
+							username: row.values[4],
+						},
+						zoom: {
+							nickname: row.values[5],
+						},
+						wakatime: {
+							api: row.values[6],
+						},
+						discord: {
+							id: row.values[7],
+						},
+					};
+					students.push(payload);
+				});
+			});
+			const newStudents = await StudentModel.insert(students);
+			res.status(200).json({ newStudents });
 		} catch (error) {
 			res.status(error.code || 500).json(error);
 		}
